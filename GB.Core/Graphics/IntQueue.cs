a7e5b640bd667c4db1c749d290d207e24d5963db
pavelsavara <pavel.savara@gmail.com>
2025-03-29 15:38:10 +0100

    IntQueue as circular buffer

--- GB.Core/Graphics/IntQueue.cs
+++ GB.Core/Graphics/IntQueue.cs
@@ -4,26 +4,72 @@ namespace GB.Core.Graphics
 {
     internal sealed class IntQueue
     {
-        private readonly List<int> _queue;
+        private readonly int[] _buffer;
+        private int _head;
+        private int _tail;
+        private int _size;
 
-        public IntQueue(int capacity) => _queue = new List<int>(capacity);
-        
+        public IntQueue(int capacity)
+        {
+            _buffer = new int[capacity];
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public int Size() => _queue.Count;
-        
-        public void Enqueue(int value) => _queue.Add(value);
+        public int Size() => _size;
+
+        public void Enqueue(int value)
+        {
+            if (_size == _buffer.Length)
+            {
+                throw new InvalidOperationException("Queue is full");
+            }
+
+            _buffer[_tail] = value;
+            _tail = (_tail + 1) % _buffer.Length;
+            _size++;
+        }
 
         public int Dequeue()
         {
-            var value = _queue[0];
-            _queue.RemoveAt(0);
+            if (_size == 0)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            var value = _buffer[_head];
+            _head = (_head + 1) % _buffer.Length;
+            _size--;
             return value;
         }
 
-        public int Get(int index) => _queue[index];
+        public int Get(int index)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
 
-        public void Clear() => _queue.Clear();
+            return _buffer[(_head + index) % _buffer.Length];
+        }
 
-        public void Set(int index, int value) => _queue[index] = value;
+        public void Clear()
+        {
+            _head = 0;
+            _tail = 0;
+            _size = 0;
+        }
+
+        public void Set(int index, int value)
+        {
+            if (index < 0 || index >= _size)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+
+            _buffer[(_head + index) % _buffer.Length] = value;
+        }
     }
 }
